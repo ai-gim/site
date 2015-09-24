167a03da8cf9e4aec7b156462b8c326a98661b50
zhangli <zhangli11@asiainfo.com>
2015-09-24 15:16:26 +0800

    CI脚本修改 安装支持

---
+++ install/install.sh
@@ -0,0 +1,29 @@
+# cat install.sh 
+#!/bin/bash
+install_dir=/usr/local/lib/gim
+if [[ -e $install_dir ]];then
+    rm -rf $install_dir/site
+else
+    mkdir -p $install_dir
+fi
+sed -n '1,/^exit 0$/!p' $0 >$install_dir/site.tar.gz
+
+cd $install_dir
+tar -xzvf site.tar.gz
+
+if [[ -e /etc/gim ]];then
+    rm -rf /etc/gim/db.conf
+    rm -rf /etc/gim/site.conf
+else
+    mkdir /etc/gim
+fi
+
+cd $install_dir/site
+cp conf/* /etc/gim
+cp init.d/gim-site /etc/rc.d/init.d
+
+chmod 755 bin/*
+chmod 755 /etc/rc.d/init.d/gim-site
+chkconfig gim-site on
+
+exit 0
